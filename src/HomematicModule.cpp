29265c461bfaa3e9a5862beb23852d1c863d6ee0
Cornelius Köpp <cornelius.koepp@fhdw.de>
2025-09-14 19:24:41 +0200

    Limit Command Help/Execution to `knx.configured()`

--- src/HomematicModule.cpp
+++ src/HomematicModule.cpp
@@ -355,63 +355,73 @@ bool HomematicModule::process_getDeviceDescription(tinyxml2::XMLDocument &doc)
 
 void HomematicModule::showHelp()
 {
-    // TODO Check and refine command definitions after first tests and extension!
-    openknx.console.printHelpLine("hmg ccuscan",    "List devices known by CCU");
-    openknx.console.printHelpLine("hmgNN",          "Device overview");
-    openknx.console.printHelpLine("hmgNN update",   "Update device state");
-    openknx.console.printHelpLine("hmgNN temp=CC",  "Set target temperature");
+    if (knx.configured())
+    {
+        // TODO Check and refine command definitions after first tests and extension!
+        openknx.console.printHelpLine("hmg ccuscan",    "List devices known by CCU");
+        openknx.console.printHelpLine("hmgNN",          "Device overview");
+        openknx.console.printHelpLine("hmgNN update",   "Update device state");
+        openknx.console.printHelpLine("hmgNN temp=CC",  "Set target temperature");
+    }
+#ifdef OPENKNX_RUNTIME_STAT
+    openknx.console.printHelpLine("hmg runtime",    "Show detailed runtime statistic");
+#endif
 }
 
 bool HomematicModule::processCommand(const std::string cmd, bool diagnoseKo)
 {
-    if (cmd.substr(0, 3) == "hmg")
-    {
-        if (cmd.length() >= 5)
-        {
-            if (cmd == "hmg ccuscan")
-            {
-                updateRssi();
-                return true;
-            }
-
-            if (!std::isdigit(cmd[3]) || !std::isdigit(cmd[4]))
-            {
-                logErrorP("=> invalid channel-number '%s'!", cmd.substr(3, 2).c_str());
-                return false;
-            }
-
-            const uint16_t channelIdx = std::stoi(cmd.substr(3, 2)) - 1;
-            if (channelIdx < HMG_ChannelCount)
-            {
-                if (cmd.length() == 5)
-                {
-                    logDebugP("=> Channel<%u> overview!", (channelIdx + 1));
-                    return _channels[channelIdx]->processCommandOverview();
-                }
-            }
-            else
-            {
-                logInfoP("=> unused channel-number %u!", channelIdx + 1);
-            }
-        }
+    // configured is not required for runtime-command
 #ifdef OPENKNX_RUNTIME_STAT
-        else if (cmd == "hmg runtime")
+    if (cmd == "hmg runtime")
+    {
+        logInfoP("HMG Runtime Statistics: (Uptime=%dms)", millis());
+        logIndentUp();
+        OpenKNX::Stat::RuntimeStat::showStatHeader();
+        char labelLoop[8 + 1] = "Ch00Loop";
+        char labelInput[8 + 1] = "Ch00Inpt";
+        for (uint8_t i = 0; i < HMG_ChannelCount; i++)
         {
-            logInfoP("HMG Runtime Statistics: (Uptime=%dms)", millis());
-            logIndentUp();
-            OpenKNX::Stat::RuntimeStat::showStatHeader();
-            char labelLoop[8 + 1] = "Ch00Loop";
-            char labelInput[8 + 1] = "Ch00Inpt";
-            for (uint8_t i = 0; i < HMG_ChannelCount; i++)
-            {
-                labelLoop[2] = labelInput[2] = '0' + i / 10;
-                labelLoop[3] = labelInput[3] = '0' + i % 10;
-                _channelLoopRuntimes[i].showStat(labelLoop, 0, true, true);
-                _channelInputRuntimes[i].showStat(labelInput, 0, true, true);
-            }
-            logIndentDown();
+            labelLoop[2] = labelInput[2] = '0' + i / 10;
+            labelLoop[3] = labelInput[3] = '0' + i % 10;
+            _channelLoopRuntimes[i].showStat(labelLoop, 0, true, true);
+            _channelInputRuntimes[i].showStat(labelInput, 0, true, true);
         }
+        logIndentDown();
+        return true;
+    }
 #endif
+
+    if (!knx.configured())
+        return false;
+
+    const size_t cmdLength = cmd.length();
+    if (cmdLength >= 5 && cmd.substr(0, 3) == "hmg")
+    {
+        if (cmd == "hmg ccuscan")
+        {
+            updateRssi();
+            return true;
+        }
+
+        if (!std::isdigit(cmd[3]) || !std::isdigit(cmd[4]))
+        {
+            logErrorP("=> invalid channel-number '%s'!", cmd.substr(3, 2).c_str());
+            return false;
+        }
+
+        const uint16_t channelIdx = std::stoi(cmd.substr(3, 2)) - 1;
+        if (channelIdx < HMG_ChannelCount)
+        {
+            if (cmdLength == 5)
+            {
+                logDebugP("=> Channel<%u> overview!", (channelIdx + 1));
+                return _channels[channelIdx]->processCommandOverview();
+            }
+        }
+        else
+        {
+            logInfoP("=> unused channel-number %u!", channelIdx + 1);
+        }
     }
     return false;
 }
